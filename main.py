4dd0b69dd9b5592bc07667b3198637105d02ac30
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-03-22 15:34:48 -0400

    add: data encoding from poistion of the user

--- main.py
+++ main.py
@@ -23,7 +23,10 @@ while True:
     faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9) 
     
     # Iterating through rectangles of detected faces 
-    for (x, y, w, h) in faces_rect: 
+    for (x, y, w, h) in faces_rect:
+        data = str.encode("P("+str(x) +","+str(y) + ")") 
+        sock.sendto(data, serverAddressPort)
+        print(data)
         cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2) 
     
     cv2.imshow('Detected faces', img)
