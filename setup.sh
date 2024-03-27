1d8219c72881380b758a3830c2079f79166aebed
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-03-27 10:29:46 -0400

    fix: script bug + update requirements.txt

--- setup.sh
+++ setup.sh
@@ -4,16 +4,16 @@
 echo "[INFO] Setting up the python environment for the OpenCV Server..."
 
 # Create the virtual enviroment, if the envirement has not been created before.
-if [ ! -d "./.venv" ]; then
+if [ ! -d "./venv" ]; then
     # Telling the user that we are creating the virtual environment 
     echo "[INFO] Creating virtual environment..."
-    mkdir .venv
-    python -m venv ./.venv
+    mkdir venv
+    python -m venv ./venv
 fi
 
 # Activating the virtual environment with the generated script
 echo "[INFO] Activating virtual environment..."
-source ./.venv/bin/activate
+source ./venv/bin/activate
 
 # Installing dependencies
 echo "[INFO] Installing dependencies from requirements.txt.."
