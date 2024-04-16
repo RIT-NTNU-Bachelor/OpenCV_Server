b21b9990061c3b4216b6cc4f8e883ef157080175
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-04-16 09:56:30 -0400

    add: constants for the depth estimation

--- src/constants.py
+++ src/constants.py
@@ -17,3 +17,17 @@ HOG_DETECTOR = dlib.get_frontal_face_detector()
 # Constants for CVZone
 CVZONE_DETECTOR = FaceMeshDetector()
 CVZONE_DETECTOR_MAX_ONE = FaceMeshDetector(maxFaces=1)
+
+
+# CVZone has a set constant for the eye indexes 
+EYE_DISTANCE_INDEX = {
+    'left_eye': 145,
+    'right_eye': 374
+}
+
+# Average distance between human eyes is 63 mm. 
+# This is 6.3 cm. 
+INTEROCULAR_DISTANCE = 6.3  
+
+# Focal length of the camera in use
+FOCAL_LENGTH = 655 
