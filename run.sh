4ed3c545e6df9995f028ea5d2bc5cf08157e8c91
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-04-16 20:21:47 -0400

    add: bash scripts for running the test and running the project

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Log the command executed in the console 
+set +xe
+
+# Running the main file in the 
+python src/main.py
---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Log the command executed in the console 
+set +xe
+
+# Running the test command in the terminal
+python -m unittest -v
