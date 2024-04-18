46648131d31b022fec90a7c66d9d44dfd9a4333a
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-04-18 08:10:31 -0400

    add: mmod to example code

--- example/example_detect_from_image.py
+++ example/example_detect_from_image.py
@@ -5,10 +5,11 @@ import cv2
 from models.code.dnn import detect_face_dnn
 from models.code.haar import detect_face_haar
 from models.code.hog import detect_face_hog
+from models.code.mmod import detect_face_mmod
 from models.code.cvzone import detect_face_cvzone
 
 # Import the detectors from src/constants.py
-from constants.model_constants import DNN_NET, CVZONE_DETECTOR_MAX_ONE, HAAR_CLASSIFIER, HOG_DETECTOR
+from constants.model_constants import DNN_NET, CVZONE_DETECTOR_MAX_ONE, HAAR_CLASSIFIER, HOG_DETECTOR, MMOD_DETECTOR
 
 # Opening a sample image
 image_path = "path/to/your/image.jpg"
@@ -19,6 +20,7 @@ faces_dnn = detect_face_dnn(img, DNN_NET)
 faces_haar = detect_face_haar(img, HAAR_CLASSIFIER)
 faces_hog = detect_face_hog(img, HOG_DETECTOR)
 faces_cvzone = detect_face_cvzone(img, CVZONE_DETECTOR_MAX_ONE)
+face_mmod = detect_face_mmod(img,MMOD_DETECTOR)
 
 # Print the amount of faces found within the image
 print(f"DNN Detected Faces: {faces_dnn}")
