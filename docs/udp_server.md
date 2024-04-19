519f23a7bc5b556fdac77d94fa8e107d51b484af
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-04-19 11:34:14 -0400

    fix: udp server docs

---
+++ docs/udp_server.md
@@ -0,0 +1,6 @@
+# UDP Server Documentation
+
+Documentation for the UDP server used in the project. This projects send the tracking data of the users face over udp. 
+Read more about the [UDP protocol here.](https://en.wikipedia.org/wiki/User_Datagram_Protocol)
+
+::: src.udp_server
--- src/udp_server.py
+++ src/udp_server.py
@@ -3,13 +3,17 @@ import socket
 def send_udp_data(sock:socket, server_address_port: tuple, data, log=False):
     """Sends data over UDP to a specified server and port.
 
-    It is used to send the coordinates over to the Unreal Engine Client. 
-    
-    Args: 
-    - sock (socket): The socket object used for communication.
-    - server_address_port(tuple): A tuple containing the server address and port. 
-    - data: The data to send.
-    - log (bool): If the function should log information in the console. Default is set to false 
+    Parameters:
+        sock (socket.socket): The socket object used for UDP communication.
+        server_address_port (tuple): A tuple containing the server address and port, formatted as (address, port).
+        data (Any): The data to be sent over UDP. This can be any data type that is serializable and supported by the socket.
+        log (bool): If set to True, the function logs the details of the sent data to the console. Defaults to False.
+
+    Note:
+        Ensure that the socket ip and port is set to match the Unreal Engine client ip and port. 
+        See the code for the [Unreal Engine Client code here.](https://github.com/RIT-NTNU-Bachelor/Unreal-facetracking-client)
+
+        Default ip and port of the client is (127.0.0.1, 5052)
     """
     encoded_data = str.encode(str(data))
     sock.sendto(encoded_data, server_address_port)
